7e166197b961a4a97317c0484e706ec5ddfc67a9
Tanzeela Ateeq <tanzeelaateeq8@gmail.com>
2026-06-11 22:57:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+//Chapter 3,
+//Q1:
+//var age = (15);
+//alert( "I am 15 years old");
+//Q2:
+//var birthYear = 1990;
+//document.writeln("My birth year is " + birthYear + "<br>");
+//document.writeln("Data type of my declared variable is " + typeof birthYear);
+//Q3:
+//  var visitorName = "John Doe" ;
+// var productTitle = "T-shirt";
+//var quantity = 5;
+
+ //document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+
+ // var customerName = "Ali";
+ // var bookName = "JavaScript";
+ // var quantity = 3;
+ // document.writeln (customerName + " ordered "  + quantity + " " + bookName +  " Books from ABC Store ");
